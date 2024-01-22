60e1c1c36b9759b2ae8259c55d7c87da3f54a973
Antoine Brunetti <antoinebrunetti@gmail.com>
2024-01-22 16:21:18 +0100

    exercice 5 tp2

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from datetime import datetime
 import pytz
+import logging
 
+logging.basicConfig(filename="traitement.log", encoding='utf-8', level=logging.DEBUG)
+logging.info(f"Lancement du traitement")
 timezone_paris = pytz.timezone('Europe/Paris')
 timezone_reunion = pytz.timezone('Indian/Reunion')
-
 def get_date_formatted(timezone):
+    if timezone is None:
+        raise ValueError("aucune timezone n'a été renseignée")
+    logging.debug(f"Demande d'heure sur le timezone : {timezone}")
     date = datetime.now(timezone)
     return date.strftime("%H:%M:%S")
-
-print(get_date_formatted(timezone_paris))
-print(get_date_formatted(timezone_reunion))
+try:
+    get_date_formatted(None) 
+except ValueError as e:
+    logging.error(e)
+logging.info(f"result {get_date_formatted(timezone_reunion)}")
